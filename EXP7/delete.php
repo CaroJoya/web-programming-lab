04788092d6df19df3041928beb8600874b11e10e
Shrushti <s75362918@gmail.com>
2026-04-08 00:23:03 +0530

    Added all 10 experiments

--- EXP7/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'db.php';
-if (isset($_POST['id'])) {
-    $stmt = $conn->prepare("DELETE FROM students WHERE id=?");
-    $stmt->bind_param("i", $_POST['id']);
-    echo $stmt->execute() ? "1" : "0";
-}
-?>
--- EXP7/update.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'db.php';
-if (isset($_POST['id'])) {
-    $stmt = $conn->prepare("UPDATE students SET name=?, email=?, phone=? WHERE id=?");
-    $stmt->bind_param("sssi", $_POST['name'], $_POST['email'], $_POST['phone'], $_POST['id']);
-    echo $stmt->execute() ? "1" : "0";
-}
-?>
